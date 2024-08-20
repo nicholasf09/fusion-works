f3782fffd086571c9376e3b867cf6054837051c2
Nicholas Fransisco <nicholasfransisco09@gmail.com>
2024-08-20 14:59:46 +0700

    image background css

--- src/pages/Upload.jsx
+++ src/pages/Upload.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import '../css/upload.css';
 
 const Upload = () => {
     const [artworkType, setArtworkType] = useState('Painting');
@@ -11,14 +12,14 @@ const Upload = () => {
 
     return (
         <div className="w-xl mx-auto m-20 p-8 bg-neutral-800 rounded-lg shadow-lg">
-            <h2 className="text-2xl font-bold text-white text-center mb-6">Upload Your Artwork</h2>
-            <p className="text-sm text-gray-300 text-center mb-6">Art is not what you see, but what you make others see. - Edgar Degas</p>
-            <form onSubmit={handleSubmit} className="gap-5 grid grid-cols-2">
-                <div>
+            <h2 className="text-2xl font-bold text-white text-center mb-6">UPLOAD YOUR WORKS</h2>
+            <p className="text-lg text-gray-300 text-center mb-6">Art is not what you see, but what you make others see. - Edgar Degas</p>
+            <form onSubmit={handleSubmit} className="md:gap-5 grid max-md:grid-cols-1 grid-cols-2">
+                <div className='max-md:col-span-2'>
                     <label className="block text-sm font-medium text-gray-300">Name</label>
                     <input type="text" required className="m-2 block w-full p-2 bg-neutral-600 text-white rounded-md focus:ring-yellow-500 focus:border-yellow-500 placeholder:italic placeholder:text-neutral-400" placeholder="What would you like this artwork to be known as?" />
                 </div>
-                <div>
+                <div className='max-md:col-span-2'>
                     <label className="block text-sm font-medium text-gray-300">Author</label>
                     <input type="text" required className="m-2 block w-full p-2 bg-neutral-600 text-white rounded-md focus:ring-yellow-500 focus:border-yellow-500 placeholder:italic placeholder:text-neutral-400" placeholder="Who is the creative mind behind this piece?" />
                 </div>
@@ -26,11 +27,11 @@ const Upload = () => {
                     <label className="block text-sm font-medium text-gray-300">Date Released</label>
                     <input type="text" required className="m-2 block w-full p-2 bg-neutral-600 text-white rounded-md focus:ring-yellow-500 focus:border-yellow-500 placeholder:italic placeholder:text-neutral-400" placeholder="When was this masterpiece unveiled?" />
                 </div>
-                <div>
+                <div className='max-md:col-span-2'>
                     <label className="block text-sm font-medium text-gray-300">Classification</label>
                     <input type="text" required className="m-2 block w-full p-2 bg-neutral-600 text-white rounded-md focus:ring-yellow-500 focus:border-yellow-500 placeholder:italic placeholder:text-neutral-400" placeholder="How would you categorize this work?" />
                 </div>
-                <div>
+                <div className='max-md:col-span-2'>
                     <label className="block text-sm font-medium text-gray-300">Type of Art</label>
                     <select value={artworkType} onChange={(e) => setArtworkType(e.target.value)} className="m-2 block w-full p-2 bg-neutral-600 text-white rounded-md focus:ring-yellow-500 focus:border-yellow-500 placeholder:italic placeholder:text-neutral-400">
                         <option value="Painting">Painting (img)</option>
@@ -42,7 +43,7 @@ const Upload = () => {
                     </select>
                 </div>
                 
-                <div>
+                <div className='max-md:col-span-2'>
                     <label className="block text-sm font-medium text-gray-300">Upload File</label>
                     <input
                         type="file"
@@ -59,7 +60,7 @@ const Upload = () => {
                         className="m-2 block w-full p-2 bg-neutral-600 text-white rounded-md focus:ring-yellow-500 focus:border-yellow-500 placeholder:italic placeholder:text-neutral-400"
                     />
                 </div>
-                <div className='col-span-2'>
+                <div className='md:col-span-2'>
                     <label className="block text-sm font-medium text-gray-300">Description</label>
                     <textarea required className="m-2 block w-full p-2 bg-neutral-600 text-white rounded-md focus:ring-yellow-500 focus:border-yellow-500 placeholder:italic placeholder:text-neutral-400" placeholder="Tell us the story behind this creation. What inspired it?"></textarea>
                 </div>
