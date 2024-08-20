3861a325d0ebc6e5daa99848184aa1967c41f48b
Nicholas Fransisco <nicholasfransisco09@gmail.com>
2024-08-20 19:49:07 +0700

    ngerapiin forms

--- src/pages/Upload.jsx
+++ src/pages/Upload.jsx
@@ -13,29 +13,33 @@ const Upload = () => {
 
     return (
         <div className={styles.uploadPage}>
-            <div className="w-xl m-20 p-8 bg-neutral-800 rounded-lg shadow-lg">
+            <div className="w-xl h-2/12 p-10 bg-neutral-800 rounded-xl shadow-lg">
                 <h2 className="text-2xl font-bold text-white text-center mb-6">UPLOAD YOUR WORKS</h2>
-                <p className="text-lg text-gray-300 text-center mb-6">Art is not what you see, but what you make others see. - Edgar Degas</p>
+                <p className="text-lg text-gray-300 text-center mb-6">Art is not what you see, but what you make others see</p>
                 <form onSubmit={handleSubmit} className="md:gap-5 grid max-md:grid-cols-1 grid-cols-2">
                     <div className='max-md:col-span-2'>
                         <label className="block text-sm font-medium text-gray-300">Name</label>
-                        <input type="text" required className="m-2 block w-full p-2 bg-neutral-600 text-white rounded-md focus:ring-yellow-500 focus:border-yellow-500 placeholder:italic placeholder:text-neutral-400" placeholder="What would you like this artwork to be known as?" />
+                        <input type="text" required className="mt-2 block w-full p-2 bg-neutral-600 text-white rounded-md focus:outline-none focus:border-yellow-500 placeholder:italic placeholder:text-neutral-400" placeholder="What would you like this artwork to be known as?" />
                     </div>
                     <div className='max-md:col-span-2'>
                         <label className="block text-sm font-medium text-gray-300">Author</label>
-                        <input type="text" required className="m-2 block w-full p-2 bg-neutral-600 text-white rounded-md focus:ring-yellow-500 focus:border-yellow-500 placeholder:italic placeholder:text-neutral-400" placeholder="Who is the creative mind behind this piece?" />
+                        <input type="text" required className="mt-2 block w-full p-2 bg-neutral-600 text-white rounded-md focus:outline-none focus:border-yellow-500 placeholder:italic placeholder:text-neutral-400" placeholder="Who is the creative mind behind this piece?" />
                     </div>
                     <div>
                         <label className="block text-sm font-medium text-gray-300">Date Released</label>
-                        <input type="text" required className="m-2 block w-full p-2 bg-neutral-600 text-white rounded-md focus:ring-yellow-500 focus:border-yellow-500 placeholder:italic placeholder:text-neutral-400" placeholder="When was this masterpiece unveiled?" />
+                        <input type="text" required className="mt-2 block w-full p-2 bg-neutral-600 text-white rounded-md focus:outline-none focus:border-yellow-500 placeholder:italic placeholder:text-neutral-400" placeholder="When was this masterpiece unveiled?" />
                     </div>
                     <div className='max-md:col-span-2'>
                         <label className="block text-sm font-medium text-gray-300">Classification</label>
-                        <input type="text" required className="m-2 block w-full p-2 bg-neutral-600 text-white rounded-md focus:ring-yellow-500 focus:border-yellow-500 placeholder:italic placeholder:text-neutral-400" placeholder="How would you categorize this work?" />
+                        <input type="text" required className="mt-2 block w-full p-2 bg-neutral-600 text-white rounded-md focus:outline-none focus:border-yellow-500 placeholder:italic placeholder:text-neutral-400" placeholder="How would you categorize this work?" />
                     </div>
                     <div className='max-md:col-span-2'>
                         <label className="block text-sm font-medium text-gray-300">Type of Art</label>
-                        <select value={artworkType} onChange={(e) => setArtworkType(e.target.value)} className="m-2 block w-full p-2 bg-neutral-600 text-white rounded-md focus:ring-yellow-500 focus:border-yellow-500 placeholder:italic placeholder:text-neutral-400">
+                        <select 
+                            value={artworkType} 
+                            onChange={(e) => setArtworkType(e.target.value)} 
+                            className="mt-2 block w-full p-2 h-12 bg-neutral-600 text-white rounded-md focus:outline-none focus:border-yellow-500 placeholder:italic placeholder:text-neutral-400"
+                        >
                             <option value="Painting">Painting (img)</option>
                             <option value="Statue">Statue (img)</option>
                             <option value="Poems">Poems (img)</option>
@@ -59,14 +63,15 @@ const Upload = () => {
                                             : 'image/*'
                             }
                             required
-                            className="m-2 block w-full p-2 bg-neutral-600 text-white rounded-md focus:ring-yellow-500 focus:border-yellow-500 placeholder:italic placeholder:text-neutral-400"
+                            className="mt-2 block w-full p-2 h-12 bg-neutral-600 text-white rounded-md focus:outline-none focus:border-yellow-500 placeholder:italic placeholder:text-neutral-400"
                         />
                     </div>
+
                     <div className='md:col-span-2'>
                         <label className="block text-sm font-medium text-gray-300">Description</label>
-                        <textarea required className="m-2 block w-full p-2 bg-neutral-600 text-white rounded-md focus:ring-yellow-500 focus:border-yellow-500 placeholder:italic placeholder:text-neutral-400" placeholder="Tell us the story behind this creation. What inspired it?"></textarea>
+                        <textarea required className="mt-2 block w-full p-2 bg-neutral-600 text-white rounded-md focus:outline-none focus:border-yellow-500 placeholder:italic placeholder:text-neutral-400" placeholder="Tell us the story behind this creation. What inspired it?"></textarea>
                     </div>
-                    <button type="submit" className="w-full py-2 px-4 bg-yellow-500 col-span-2 text-white font-semibold rounded-md shadow-md shadow-yellow-500/50 hover:outline-none  focus:ring-yellow-500">
+                    <button type="submit" className="w-full py-2 px-4 bg-yellow-500 col-span-2 text-white font-semibold rounded-md shadow-lg shadow-yellow-500 hover:outline-none  focus:outline-none">
                         Upload
                     </button>
                 </form>
